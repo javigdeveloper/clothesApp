11429616ea60e1c6fb54494c00c18670099fd382
Javier <javigdeveloper@gmail.com>
2020-03-12 15:05:17 -0500

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_204300) do
+ActiveRecord::Schema.define(version: 2020_02_10_164728) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2020_02_01_204300) do
   create_table "garments", force: :cascade do |t|
     t.string "prenda"
     t.decimal "precio"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "owners", force: :cascade do |t|
+    t.string "name"
+    t.integer "garment_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
